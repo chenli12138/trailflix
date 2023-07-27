931444af422d42f6685ce3db156e9efab0cd1315
chenli12138 <lgf130031@gmail.com>
2023-07-28 09:21:49 +1000

    CSR Ver

--- app/page.js
+++ app/page.js
@@ -1,15 +1,32 @@
+"use client";
 import Hero from "./components/Hero";
 import Row from "./components/Row";
+import { useState, useEffect } from "react";
 
-export default async function Home() {
-  const res = await fetch("http://trailflix-pi.vercel.app/api/trend");
-  const heros = await res.json();
-  const movies = heros.results;
-  // Create different Hero pages
-  const hero = movies[Math.floor(Math.random() * movies.length)];
+export default function Home() {
+  const [hero, setData] = useState(null);
+
+  // Loading Movie Data
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Send a request to your API route
+        const response = await fetch("/api/trend"); // Relative URL for the API route
+        const heros = await response.json();
+        const movies = heros.results;
+        // Create different Hero pages
+        const hero = movies[Math.floor(Math.random() * movies.length)];
+        setData(hero);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Hero hero={hero} />
+      {hero && <Hero hero={hero} />}
       <Row rowID={1} fetchURL={"trend"} rowName={"Trending"} />
       <Row rowID={2} fetchURL={"coming"} rowName={"Upcoming"} />
       <Row rowID={3} fetchURL={"top"} rowName={"Top Rated"} />
